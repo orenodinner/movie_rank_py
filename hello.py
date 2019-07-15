a9b4acc9a80ad331527dae75617c82cfcd860bb0
orenodinner <orenodinner@gmail.com>
2019-07-15 14:07:25 +0900

    評価フォルダ移動機能追加

--- hello.py
+++ hello.py
@@ -5,23 +5,44 @@ import glob
 import shutil
 
 app = Flask(__name__)
+
+
 all_list =[]
 num_count = 0
 
+p = Path("static/image/input")
+all_list = list(p.glob("*.jpg"))
+app.logger.debug(all_list)
+
+
 @app.route('/')
 def hello(name=None):
-    p = Path("static/image/input")
-    all_list = list(p.glob("*.jpg"))
-    app.logger.debug(all_list)
-    shutil.copy(all_list[0],"static/image/temp/temp.jpg")
+    global num_count
+    global all_list
+    #shutil.copy(str(all_list[num_count]),"static/image/temp/temp.jpg")
+    input_path = str(all_list[num_count])
     
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', input_path=input_path)
 
 @app.route('/next' ,methods=['POST'])
-def rank():
+def rank(name=None):
+    global num_count
+    global all_list
     if request.method == "POST":
         res = request.form["bt"]
-    return res
+        if res =="good":
+            move_path = "static/image/good/"
+        elif res == "midle":
+            move_path = "static/image/midle/"
+        elif res == "bad":
+            move_path = "static/image/bad/"
+            pass                
+        shutil.move(str(all_list[num_count]),move_path)
+        num_count = num_count +1
+        #shutil.copy(str(all_list[num_count]),"static/image/temp/temp.jpg")
+        input_path = str(all_list[num_count])
+
+    return render_template('hello.html', input_path = input_path)
 
 if __name__ == '__main__':
     app.run(debug=True , host='0.0.0.0', port=5000)
